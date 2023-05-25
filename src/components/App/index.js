f21f2f1bc140e9c88398762af261f2de18718a0d
Alix-Large <largealix@gmail.com>
2023-05-25 09:32:45 +0200

    render

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import './style.scss';
+
+function App() {
+    return (
+        <h1 className="title">
+            <span>Hello</span>
+            <span>world!</span>
+        </h1>
+    );
+}
+
+export default App;
