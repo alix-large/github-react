727e1c1b7ac01e51ae366a3a86b5ae1221c1ea7e
Alix-Large <largealix@gmail.com>
2023-05-25 21:26:28 +0200

    State isLoading ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,11 +9,14 @@ import Results from '../Results';
 function App() {
 
     const [list, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
 
     const fetchRepos = async(search) => {
             try {
+                setIsLoading(true);//dès qu'on fait appel à l'api, on déclenche un nouveau state pour dire qu'on est en train de charger
                 const response = await fetch("https://api.github.com/search/repositories?q=react" + search);
                 const data = await response.json();
+                setIsLoading(false);//dès qu'on a la réponse de l'api, on repasse à l'état false
                 setList(data.items);//déclenche un nouveau state, la nouvelle valeur
             } catch (error) {
                 console.error(error);
@@ -26,12 +29,13 @@ function App() {
     }, []);    
 
     return (
-    <main className = "app">
-        <h1>Rechercher des dépots sur Github</h1>
-        <Form fetchRepos={fetchRepos}/>
-        <Results 
-            list={list}
-        />
+    <main>
+        <div className= "app">
+            <h1>Rechercher des dépots sur Github</h1>
+            <Form fetchRepos={fetchRepos}/>
+            {isLoading && <p>Veuillez patientez</p>}
+            {!isLoading &&  <Results list={list}/>}
+        </div>
     </main>
     );
 }
