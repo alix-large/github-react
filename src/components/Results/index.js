17dd3df6c200a12d9482045ba62d58d1eac4678e
Alix-Large <largealix@gmail.com>
2023-05-30 18:45:44 +0200

    fix results component css problem

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -6,18 +6,20 @@ import Card from '../Card';
 function Results({list}) {
   console.log(list);
   return (
-    <section className="results">
+    <section>
       <h2>{getTitleByResultsNumber(list)}</h2>
+        <div  className="results">
         {list.map((repo) => (
-        <Card 
-            key = {repo.id}
-            title={repo.full_name}
-            desc={repo.description}
-            avatar = {repo.owner.avatar_url}
-            url = {repo.html_url}
-            open_issues ={repo.open_issues}
-          />
-        ))}   
+          <Card 
+              key = {repo.id}
+              title={repo.full_name}
+              desc={repo.description}
+              avatar = {repo.owner.avatar_url}
+              url = {repo.html_url}
+              open_issues ={repo.open_issues}
+            />
+          ))} 
+        </div>  
     </section>
   );
 }
